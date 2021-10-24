6e26c7965df3a6b8ed278a7f65dedb51883477e8
DevNebulaP <puttipong.nebula@gmail.com>
2021-10-24 16:38:38 +0700

    Update moderation commands to require permissions

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -8,21 +8,25 @@ class Moderation(commands.Cog):
         self.client = client
 
     @commands.command(aliases=['clr'])
+    @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount=10):
         """Clear messages.\tArguments: amount(default=10)"""
         await ctx.channel.purge(limit=amount+1)
 
     @commands.command()
+    @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member : discord.Member, *, reason=None):
         """Kicks a user.\tArguments: member"""
         await member.kick(reason=reason)
 
     @commands.command()
+    @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member : discord.Member, *, reason=None):
         """Bans a user.\tArguments: member"""
         await member.ban(reason=reason)
 
     @commands.command()
+    @commands.has_permissions(ban_members=True)
     async def banned(self, ctx):
         """Shows banned users.\tArguments: None"""
         banned_users = await ctx.guild.bans()
@@ -30,6 +34,7 @@ class Moderation(commands.Cog):
         await ctx.send(", ".join(banned_users))
 
     @commands.command()
+    @commands.has_permissions(ban_members=True)
     async def unban(self, ctx, *, member):
         """Unbans a user.\tArguments: member"""
         banned_users = await ctx.guild.bans()
