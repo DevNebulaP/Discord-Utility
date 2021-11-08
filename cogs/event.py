b824c355f8315b27dbc4f8be72787dd2ea4de814
DevNebulaP <puttipong.nebula@gmail.com>
2021-11-08 20:30:45 +0700

    Added a voting feature

--- cogs/event.py
+++ cogs/event.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 from discord import Embed
 import json
 
+
 class Event(commands.Cog):
 
     def __init__(self, client):
@@ -17,9 +18,9 @@ class Event(commands.Cog):
         """Proposes an event that can be reacted to.\t Arguments: title"""
         await ctx.message.delete()
         embed = Embed(
-            title = title,
-            description = f"{ctx.author} has proposed an event.",
-            colour = Colour.green()
+            title=title,
+            description=f"{ctx.author} has proposed an event.",
+            colour=Colour.green()
         )
 
         embed.set_footer(text='React below to respond to this event invite')
@@ -37,15 +38,15 @@ class Event(commands.Cog):
             data = json.load(json_read)
 
             new_event = {
-                "message_id" : msg.id,
-                "channel_id" : msg.channel.id
+                "message_id": msg.id,
+                "channel_id": msg.channel.id
             }
 
             data.append(new_event)
 
         with open("json_files/event.json", "w") as json_write:
             json.dump(data, json_write, indent=4)
-    
+
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         """React when a reaction is added"""
@@ -64,16 +65,22 @@ class Event(commands.Cog):
                         new_embed = msg.embeds[0]
                         if payload.emoji.name == "✅":
                             field = new_embed.fields[0].value
-                            if field == "None": field = ""
-                            new_embed.set_field_at(index=0, name="Accepted", value=field+"\n"+payload.member.name)             
+                            if field == "None":
+                                field = ""
+                            new_embed.set_field_at(
+                                index=0, name="Accepted", value=field+"\n"+payload.member.name)
                         elif payload.emoji.name == "❌":
                             field = new_embed.fields[1].value
-                            if field == "None": field = ""
-                            new_embed.set_field_at(index=1, name="Declined", value=field+"\n"+payload.member.name)
+                            if field == "None":
+                                field = ""
+                            new_embed.set_field_at(
+                                index=1, name="Declined", value=field+"\n"+payload.member.name)
                         elif payload.emoji.name == "❔":
                             field = new_embed.fields[2].value
-                            if field == "None": field = ""
-                            new_embed.set_field_at(index=2, name="Maybe", value=field+"\n"+payload.member.name)
+                            if field == "None":
+                                field = ""
+                            new_embed.set_field_at(
+                                index=2, name="Maybe", value=field+"\n"+payload.member.name)
                         await msg.edit(embed=new_embed)
                         return
 
@@ -93,22 +100,29 @@ class Event(commands.Cog):
                         field = new_embed.fields[0].value
                         field = field.split("\n")
                         field.remove(user.name)
-                        if not len(field): field = ["None"]
-                        new_embed.set_field_at(index=0, name="Accepted", value="\n".join(field))             
+                        if not len(field):
+                            field = ["None"]
+                        new_embed.set_field_at(
+                            index=0, name="Accepted", value="\n".join(field))
                     elif payload.emoji.name == "❌":
                         field = new_embed.fields[1].value
                         field = field.split("\n")
                         field.remove(user.name)
-                        if not len(field): field = ["None"]
-                        new_embed.set_field_at(index=1, name="Declined", value="\n".join(field))
+                        if not len(field):
+                            field = ["None"]
+                        new_embed.set_field_at(
+                            index=1, name="Declined", value="\n".join(field))
                     elif payload.emoji.name == "❔":
                         field = new_embed.fields[2].value
                         field = field.split("\n")
                         field.remove(user.name)
-                        if not len(field): field = ["None"]
-                        new_embed.set_field_at(index=2, name="Maybe", value="\n".join(field))
+                        if not len(field):
+                            field = ["None"]
+                        new_embed.set_field_at(
+                            index=2, name="Maybe", value="\n".join(field))
                     await msg.edit(embed=new_embed)
                     return
+
 
 def setup(client):
     client.add_cog(Event(client))
---
+++ cogs/vote.py
@@ -0,0 +1,102 @@
+from discord.ext import commands
+from discord import Embed, emoji, message
+from discord import Colour
+from discord.ext.commands import context
+import json
+
+
+class Vote(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.has_permissions(send_messages=True)
+    async def vote(self, ctx, title, *options):
+        """Sets up a vote that can be reacted to.\t Arguments: title, options(multiple: at least 2)"""
+        if len(options) < 2 or len(options) > 10:
+            await ctx.send("There must be at least 2 and no more than 10 options to vote")
+            return
+
+        emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣",
+                  "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
+
+        await ctx.message.delete()
+        options = [
+            f"**{i+1}. {options[i]}:\t**`0`" for i in range(len(options))]
+        contents = "\n".join(options)
+        embed = Embed(
+            title=title,
+            description=f"{ctx.author} started a vote.\n{contents}",
+            colour=Colour.orange()
+        )
+
+        embed.set_footer(text='React below to vote')
+        embed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
+
+        msg = await ctx.send(embed=embed)
+
+        for i in range(len(options)):
+            await msg.add_reaction(emojis[i])
+
+        with open("json_files/vote.json") as json_read:
+            data = json.load(json_read)
+
+            new_vote = {
+                "message_id": msg.id,
+                "channel_id": msg.channel.id
+            }
+
+            data.append(new_vote)
+
+        with open("json_files/vote.json", "w") as json_write:
+            json.dump(data, json_write, indent=4)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        """React when a reaction is added"""
+        if payload.member.bot:
+            pass
+
+        else:
+
+            with open("json_files/vote.json") as event_file:
+
+                data = json.load(event_file)
+                for i in data:
+                    if i['message_id'] == payload.message_id:
+                        emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣",
+                                  "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
+                        index = emojis.index(payload.emoji.name) + 1
+                        channel = self.client.get_channel(id=i['channel_id'])
+                        msg = await channel.fetch_message(id=i['message_id'])
+                        new_embed = msg.embeds[0]
+                        contents = new_embed.description.split("\n")
+                        contents[index] = contents[index][:contents[index].find(
+                            "`")+1] + str(int(contents[index][contents[index].find("`")+1:-1])+1) + "`"
+                        new_embed.description = "\n".join(contents)
+                        await msg.edit(embed=new_embed)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        """Reacts when a reaction is removed"""
+        with open("json_files/vote.json") as event_file:
+
+            data = json.load(event_file)
+            for i in data:
+                if i['message_id'] == payload.message_id:
+                    emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣",
+                              "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
+                    index = emojis.index(payload.emoji.name) + 1
+                    channel = self.client.get_channel(id=i['channel_id'])
+                    msg = await channel.fetch_message(id=i['message_id'])
+                    new_embed = msg.embeds[0]
+                    contents = new_embed.description.split("\n")
+                    contents[index] = contents[index][:contents[index].find(
+                        "`")+1] + str(int(contents[index][contents[index].find("`")+1:-1])-1) + "`"
+                    new_embed.description = "\n".join(contents)
+                    await msg.edit(embed=new_embed)
+
+
+def setup(client):
+    client.add_cog(Vote(client))
