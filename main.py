5678e33e81db33502a5a61f93ac2ffde7fb06d49
DevNebulaP <puttipong.nebula@gmail.com>
2021-12-08 19:32:29 +0700

    removed cog loading commands

--- main.py
+++ main.py
@@ -16,41 +16,18 @@ async def on_ready():
     print(f"logged in at {time.ctime(time.time())} as {client.user}")
 
 
-
 @slash.slash(name="hi",
-            description="Says Hello!!")
+             description="Says Hello!!")
 async def hi(ctx):
     """Says Hello!!.\tArguments: None"""
     await ctx.send("Hello!!")
 
 
 @slash.slash(name="latency",
-            description="Measures the bot ping.")
+             description="Measures the bot ping.")
 async def latency(ctx):
     """Measures the bot ping.\tArguments: None"""
     await ctx.send(f"latency is around {round(client.latency * 1000)} ms.")
-
-
-@client.command()
-async def load(ctx, extension):
-    """Loads a cog extension.\tArguments: extension"""
-    client.load_extension(f"cogs.{extension}")
-    await ctx.send(f"Loaded the {extension} cog")
-
-
-@client.command()
-async def unload(ctx, extension):
-    """Loads a cog extension.\tArguments: extension"""
-    client.unload_extension(f"cogs.{extension}")
-    await ctx.send(f"Unloaded the {extension} cog")
-
-
-@client.command()
-async def reload(ctx, extension):
-    """Reloads a cog extension.\tArguments: extension"""
-    client.unload_extension(f"cogs.{extension}")
-    client.load_extension(f"cogs.{extension}")
-    await ctx.send(f"Reloaded the {extension} cog")
 
 
 for file in os.listdir("./cogs"):
